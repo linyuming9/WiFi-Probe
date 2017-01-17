a988fc968147805acc89ce5b0c251cae0cde19cc
linyuming9 <linyuming9@qq.com>
2017-01-17 10:32:24 +0800

    Required packages

---
+++ package.sh
@@ -0,0 +1,7 @@
+opkg update
+
+opkg install kmod-usb-storage kmod-fs-ext4 kmod-fs-vfat kmod-nls-cp437 kmod-nls-iso8859-1  # For USB storage device
+opkg install python-light # Intsall python
+mkdir -p /mnt/usb # A new folder to mount to
+
+opkg install tcpdump # Install tcpdump
